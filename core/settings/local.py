5115b22d275ec77b98827a68e2e81560ec75ad38
AlexCuenca99 <alex-patricio1999@hotmail.com>
2023-08-23 10:56:53 -0500

    Extend JWT lifetime

--- core/settings/local.py
+++ core/settings/local.py
@@ -77,7 +77,7 @@ CORS_ALLOW_CREDENTIALS = True
 
 # SimpleJWT
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=7),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
